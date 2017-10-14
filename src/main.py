d965f3c102a9ca5fd1f6adb862d340ebb76c09e7
nika <ndogh13@freeuni.edu.ge>
2017-10-14 19:30:57 +0400

    Add parsing relations

--- src/main.py
+++ src/main.py
@@ -2,26 +2,30 @@ from collections import defaultdict
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
-FILE_NAME = 'sample.osm'
+FILE_NAME = 'sapmle.osm'
 
 
 def fix_types(data):
     data['id'] = int(data['id'])
-    if 'visible' in data:
-        data['visible'] = data['visible'] == 'true'
     data['version'] = int(data['version'])
     data['changeset'] = int(data['changeset'])
     data['timestamp'] = datetime.strptime(data['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
     data['uid'] = int(data['uid'])
-    data['lat'] = float(data['uid'])
-    data['lon'] = float(data['uid'])
 
+    if 'visible' in data:
+        data['visible'] = data['visible'] == 'true'
+    if 'lat' in data:
+        data['lat'] = float(data['lat'])
+    if 'lon' in data:
+        data['lon'] = float(data['lon'])
     if 'nodes' in data:
         data['nodes'] = list(map(int, data['nodes']))
+    if 'ways' in data:
+        data['ways'] = list(map(int, data['ways']))
     return data
 
 
-def parse_elem_node_or_way(elem, tree):
+def parse_elem(elem, tree):
     elem_tag = elem.tag
     attributes = elem.attrib
     children = defaultdict(list)
@@ -33,7 +37,7 @@ def parse_elem_node_or_way(elem, tree):
         if event == 'start':
             children[elem.tag].append(elem.attrib)
 
-    elem_data = {'type': elem_tag}
+    elem_data = {'elem_tag': elem_tag}
 
     for tag in children['tag']:
         key, value = tag['k'], tag['v']
@@ -42,14 +46,25 @@ def parse_elem_node_or_way(elem, tree):
     if len(children['nd']) > 0:
         elem_data['nodes'] = [x['ref'] for x in children['nd']]
 
+    if len(children['member']) > 0:
+        nodes = []
+        ways = []
+        for member in children['member']:
+            if member['type'] == 'node':
+                nodes.append(member['ref'])
+            elif member['type'] == 'way':
+                ways.append(member['ref'])
+        elem_data['nodes'] = nodes
+        elem_data['ways'] = ways
+
     elem_data = {**attributes, **elem_data}
     elem_data = fix_types(elem_data)
     return elem_data
 
 
 def parse_elems(elem, tree):
-    if elem.tag in ['node', 'way']:
-        yield parse_elem_node_or_way(elem, tree)
+    if elem.tag in ['node', 'way', 'relation']:
+        yield parse_elem(elem, tree)
     else:
         while next(tree)[0] != 'end':
             pass
@@ -68,7 +83,7 @@ def parse_all_elems(tree):
 def main():
     tree = ET.iterparse(FILE_NAME, events=('start', 'end'))
     for data in parse_all_elems(tree):
-        print(data)
+        print(data['elem_tag'])
 
 
 if __name__ == '__main__':
